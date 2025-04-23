e29e85547653373aeaf9bfbc0ebeb2fb51dc6156
swagatobhaskar <bhaskarswagato@gmail.com>
2025-04-23 15:14:09 +0530

    Add: Logic to separate dev and prod database.

--- database.py
+++ database.py
@@ -1,11 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///./test.db"
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+ENV = os.getenv("ENV", "development")
+
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URI, connect_args={'check_same_thread': False}
+    DATABASE_URL, connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
