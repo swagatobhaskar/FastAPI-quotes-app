899e96c5fe4b05347738ef597efc8680cf3e1fa4
swagatobhaskar <bhaskarswagato@gmail.com>
2025-04-23 13:43:06 +0530

    Edit: Changed sample Quote data format according to SQLAlchemy.

--- seed_db.py
+++ seed_db.py
@@ -17,14 +17,24 @@ if quote_count == 0:
     print("** No quote found! Seeding Database..")
 
     quotes = [
-        {'id': 1, 'author': 'bob', 'text': 'Awwwu..'},
-        {'id': 2, 'author': 'john hoe', 'text': 'I\'m gay'},
-        {'id': 3, 'author': 'bruce lee', 'text': 'Don\'t pray for an easy life, pray for the endurance to live a hard life.'}
-        ]
-
+        Quote(id=1, author='bob', text='Awwwu..'),
+        Quote(id=2, author='john hoe', text='I\'m gay'),
+        Quote(
+            id=3,
+            author='bruce lee',
+            text='Don\'t pray for an easy life, pray for the endurance to live a hard life.'
+        ),
+        Quote(
+            id=4,
+            author='marie curie',
+            text='Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, \
+                  so that we may fear less.'
+        )
+    ]
+    
     db.add_all(quotes)
     db.commit()
-    print("Seeded test.db with sample quotes.")
+    print("✅️ Seeded test.db with sample quotes.")
 
 else:
     print(f"⚠️  Database already has {quote_count} quote(s). Skipping seed.")
